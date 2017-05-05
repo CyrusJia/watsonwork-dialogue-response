c6f0268028a3425405dbba7e6bbdbc4e4575cc0f
Cyrus Jia <cyrus.jia@ibm.com>
2017-05-05 15:07:45 -0700

    Add state saving demo

--- src/app.js
+++ src/app.js
@@ -18,104 +18,118 @@ import debug from 'debug';
 const log = debug('watsonwork-debug-app');
 
 // Handle events sent to the annotation Webhook at /webhook
-export const webhook = (appId, store, wuser, wpassword, token) =>
-  (req, res) => {
-    // log('Received body %o', req.body);
+export const webhook = (appId, store, token) =>
+(req, res) => {
+  // log('Received body %o', req.body);
 
-    // Get the space containing the conversation that generated the event
-    const spaceId = req.body.spaceId;
+  // Get the space containing the conversation that generated the event
+  const spaceId = req.body.spaceId;
 
-    // A utility function that sends a message back to the conversation in
-    // that space
-    const send = (message) => {
-      messages.send(spaceId,
-        message.title, message.text, message.actor, token());
-    };
+  // A utility function that sends a message back to the conversation in
+  // that space
+  const send = (message) => {
+    messages.send(spaceId,
+      message.title, message.text, message.actor, token());
+  };
 
     // Respond to the Webhook right away, as any response messages will
     // be sent asynchronously
-    res.status(201).end();
+  res.status(201).end();
 
-    // Handle messages identified as action requests
-    events.onAnnotation(req.body, appId, token,
-      (action, focus, message, user) => {
+    // Handle events
+  events.onEvent(req.body, appId, token,
+      (message, user) => {
 
         // Run with any previously saved action state
         state.run(spaceId, user.id, store, (astate, cb) => {
 
+          // state demonstration
+          // if(astate.hasSentAlready)
+          // {
+          //   send(customMessage('I have already received my first event!'));
+          // }
+          // else {
+          //   send(customMessage('Hello! I just received my first event!'));
+          //   astate.hasSentAlready = true;
+          // }
 
           // Return the new action state
           cb(null, astate);
         });
       });
 
-  };
+};
 
-// Create Express Web app
+// Custom message formatter
+const customMessage = (msg) => ({
+  text: util.format(msg)
+});
+
+
+    // Create Express Web app
 export const webapp =
-  (appId, secret, whsecret, store, wuser, wpassword, cb) => {
-    // Authenticate the app and get an OAuth token
-    oauth.run(appId, secret, (err, token) => {
-      if(err) {
-        cb(err);
-        return;
-      }
+    (appId, secret, whsecret, store, cb) => {
+      // Authenticate the app and get an OAuth token
+      oauth.run(appId, secret, (err, token) => {
+        if(err) {
+          cb(err);
+          return;
+        }
 
-      // Return the Express Web app
-      cb(null, express()
+        // Return the Express Web app
+        cb(null, express()
 
         // Configure Express route for the app Webhook
         .post('/webhook',
 
-          // Verify Watson Work request signature and parse request body
-          bparser.json({
-            type: '*/*',
-            verify: sign.verify(whsecret)
-          }),
+        // Verify Watson Work request signature and parse request body
+        bparser.json({
+          type: '*/*',
+          verify: sign.verify(whsecret)
+        }),
 
-          // Handle Watson Work Webhook challenge requests
-          sign.challenge(whsecret),
+        // Handle Watson Work Webhook challenge requests
+        sign.challenge(whsecret),
 
-          // Handle Watson Work Webhook events
-          webhook(appId, state.store(store), wuser, wpassword, token)));
-    });
-  };
+        // Handle Watson Work Webhook events
+        webhook(appId, state.store(store), token)));
+      });
+    };
 
-// App main entry point
+    // App main entry point
 const main = (argv, env, cb) => {
-  // Create Express Web app
+      // Create Express Web app
   webapp(
-    env.APP_ID,
-    env.APP_SECRET,
-    env.WEBHOOK_SECRET,
-    env.STORE,
-    env.TWC_USER,
-    env.TWC_PASSWORD, (err, app) => {
-      if(err) {
-        cb(err);
-        return;
-      }
-
-      if(env.PORT) {
-        // In a hosting environment like Bluemix for example, HTTPS is
-        // handled by a reverse proxy in front of the app, just listen
-        // on the configured HTTP port
-        log('HTTP server listening on port %d', env.PORT);
-        http.createServer(app).listen(env.PORT, cb);
-      }
-
-      else
-        // Listen on the configured HTTPS port, default to 443
-        ssl.conf(env, (err, conf) => {
+        env.APP_ID,
+        env.APP_SECRET,
+        env.WEBHOOK_SECRET,
+        env.STORE,
+        (err, app) => {
           if(err) {
             cb(err);
             return;
           }
-          const port = env.SSLPORT || 443;
-          log('HTTPS server listening on port %d', port);
-          https.createServer(conf, app).listen(port, cb);
+
+          if(env.PORT) {
+            // In a hosting environment like Bluemix for example, HTTPS is
+            // handled by a reverse proxy in front of the app, just listen
+            // on the configured HTTP port
+            log('HTTP server listening on port %d', env.PORT);
+            http.createServer(app).listen(env.PORT, cb);
+          }
+
+          else
+          // Listen on the configured HTTPS port, default to 443
+          ssl.conf(env, (err, conf) => {
+            if(err) {
+              cb(err);
+              return;
+            }
+            const port = env.SSLPORT || 443;
+            log('HTTPS server listening on port %d', port);
+            https.createServer(conf, app).listen(port, cb);
+          });
         });
-    });
 };
 
 if (require.main === module)
--- src/events.js
+++ src/events.js
@@ -28,54 +28,13 @@ const callback = (evt, appId, info, annotation, token, cb) => {
   });
 };
 
-// Return the action identified in an annotation event
-export const onAction = (evt, appId, token, cb) => {
-  // Check for a focus annotation
-  if(evt.type === 'message-annotation-added' &&
-    evt.annotationType === 'message-focus') {
 
-    // Call back with any action found on the focus annotation
-    const focus = JSON.parse(evt.annotationPayload);
-    if(focus.applicationId === appId) {
-      const action = focus.actions && focus.actions[0];
-      if(action) {
-        log('Idenfified action %s', action);
-        callback(evt, appId, action, focus, token, cb);
-      }
-    }
-  }
-};
-
-// Return the next step in an action, determined from a user input
-export const onActionNextStep = (evt, appId, token, cb) => {
-  // Check for a focus annotation
-  if(evt.type === 'message-annotation-added' &&
-    evt.annotationType === 'message-focus') {
-
-    // Call back with any action next step found on the focus annotation
-    const focus = JSON.parse(evt.annotationPayload);
-    if(focus.applicationId === appId) {
-      const payload = JSON.parse(focus.payload);
-      const next = (payload.actionNextSteps || [])[0];
-      if(next) {
-        log('Idenfified action next step %s', next);
-        callback(evt, appId, next, focus, token, cb);
-      }
-    }
-  }
-};
-
-// Return the entities recognized in an annotation event
-export const onEntities = (evt, appId, token, cb) => {
-  // Check for an entities annotation
-  if(evt.type === 'message-annotation-added' &&
-    evt.annotationType === 'message-nlp-entities') {
-
-    // Call back with any recognized entities
-    const nlp = JSON.parse(evt.annotationPayload);
-    if(nlp.entities.length) {
-      log('Idenfified entities %o', nlp.entities);
-      callback(evt, appId, nlp.entities, nlp, token, cb);
-    }
-  }
+// Return the event identified
+export const onEvent = (evt, appId, token, cb) => {
+  log('Event received...');
+  log(evt);
+  var info = evt;
+  var annotation = evt;
+  // callback
+  callback(evt, appId, info, annotation, token, cb);
 };
